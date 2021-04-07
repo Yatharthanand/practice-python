586a04e80425ec4aaf199140f915f4dd6ba63e6d
Yatharthanand <Yatharthanand1995@gmail.com>
2021-04-07 17:55:49 +0530

    7th April

---
+++ 37_practice_Star.py
@@ -0,0 +1,11 @@
+def print_star_triangle(n):
+    for i in range(n):
+        print("*",end="")
+    print()
+
+def main():
+    for i in range(5,0,-1):
+        print_star_triangle(i)
+
+main()
+
---
+++ 39_sum_of_naturals_using_loops.py
@@ -0,0 +1,17 @@
+def get_sum(n):
+    sum= 0
+    for i in range(n+1):
+        sum = sum + i
+    return sum
+    
+def main():
+    n = int(input("Enter n:"))
+    sum = get_sum(n)
+    print(f"Sum of first {n} Natural numbers = {sum}")
+
+main()
+
+"""
+i
+--------------
+"""
